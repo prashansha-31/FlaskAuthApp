75be2feccd8983337a729a10932ba41f7ce69b0f
Prashansha Maheshwari <prashansha.maheshwari_cs.aim24@gla.ac.in>
2026-02-21 00:59:34 +0530

    Improved DB config for Render

--- app.py
+++ app.py
@@ -8,7 +8,10 @@ app.secret_key = "secret_key"
 
 # Database Configuration (Render Safe)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "database.db")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL") or \
+    "sqlite:///" + os.path.join(basedir, "database.db")
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
